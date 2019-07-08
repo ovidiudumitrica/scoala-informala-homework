6f987cce55d8c5eb15df000fdcf054d84b55f346
ovidiudumitrica <adecvatu@yahoo.com>
2019-07-08 20:08:17 +0300

    Added homework IDEA Project

--- src/ro/siit/tema1/TestClass.java
+++ src/ro/siit/tema1/TestClass.java
@@ -3,5 +3,8 @@ package ro.siit.tema1;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("This is a test");
+        for (int i=0; i < 10; i++) {
+            System.out.println("Merge");
+        }
     }
 }
